45b40a94cf32565d25a677e755a077afaf01c988
Ricky-L21 <71520397+Ricky-L21@users.noreply.github.com>
2021-05-11 00:58:40 -0400

    well..........next time

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,15 @@
 import { DOMSelectors } from "./DOM";
 import { genres } from "./genre";
 
-const key = "156577b197f89908dd404604e807fba3";
+const key = "b2e5f5f9-a41f-4466-833d-a09cd17f4fd7";
 
 
 const query = async function () {
     try {
-        const response = await fetch(`https://dog.ceo/api/breed/shiba/images/random/10`);
+        const response = await fetch(`https://holidayapi.com/v1/holidays?pretty&key=b2e5f5f9-a41f-4466-833d-a09cd17f4fd7&country=US&year=2020`);
         const data = await response.json();
         console.log(data);
-        data.results.forEach((dog)=>{
+        data.holidays.forEach((holiday)=>{
             DOMSelectors.grid.insertAdjacentHTML("beforeend", `
             <div class="movie-card">
             <div class="movie-card-front">
@@ -18,30 +18,27 @@ const query = async function () {
                 alt=""
                 class="poster"
               />
+              <h3 class="movie-card-header">${holiday.name}</h3>
             </div>
             <div class="movie-card-back">
-              <h3 class="movie-card-header">The Dark Knight</h3>
+              
               <div class="score-box">
-                <p class="user-score">Community Score</p>
-                <p class="user-score">8.4</p>
+                <p class="user-score">Public Holiday</p>
+                <p class="user-score">${holiday.public}</p>
               </div>
     
               <div class="release-box">
-                <p class="release-date">Released</p>
-                <p class="release-date">2020-06-12</p>
+                <p class="release-date">Date</p>
+                <p class="release-date">${holiday.date}</p>
+                <p class="release-date">${holiday.weekday.date.name}</p>
               </div>
-    
-              <div class="movie-genres">
-                <li class="movie-genre">Sci-Fi</li>
-                <li class="movie-genre">Fantasy</li>
-                <li class="movie-genre">Horror</li>
-              </div>
+
             </div>
           </div>`);
         })
     } catch (error) {
         console.log(error);
-        alert("SOMETHING WRONG BOSS")
+        alert("SOMETHING WRONG BOSS");
     }
 
 };
