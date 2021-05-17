a568d682c778891286efd57132c1131fa8f6cc7e
Ricky-L21 <71520397+Ricky-L21@users.noreply.github.com>
2021-05-17 01:52:28 -0400

    Work on search next

--- src/js/DOM.js
+++ src/js/DOM.js
@@ -1,5 +1,7 @@
 const DOMSelectors = {
   grid: document.querySelector(".movie-grid"),
+  searchForm: document.getElementById("search-from"),
+  searchArea: document.getElementById("search-area"),
 };
 
 export { DOMSelectors };
--- src/js/index.js
+++ src/js/index.js
@@ -8,21 +8,19 @@ const query = async function () {
     try {
         const response = await fetch(`https://holidayapi.com/v1/holidays?pretty&key=b2e5f5f9-a41f-4466-833d-a09cd17f4fd7&country=US&year=2020`);
         const data = await response.json();
-        console.log(data);
-        data.holidays.forEach((holiday)=>{
-            DOMSelectors.grid.insertAdjacentHTML("beforeend", `
-            <div class="movie-card">
+        console.log(data.holidays);
+        data.holidays.forEach((holiday) => {
+            DOMSelectors.grid.insertAdjacentHTML(
+              "beforeend", 
+              `<div class="movie-card">
             <div class="movie-card-front">
-              <img
-                src="https://image.tmdb.org/t/p/w300/r7vmZjiyZw9rpJMQJdXpjgiCOk9.jpg"
-                alt=""
-                class="poster"
-              />
+              
               <h3 class="movie-card-header">${holiday.name}</h3>
             </div>
             <div class="movie-card-back">
               
               <div class="score-box">
+              
                 <p class="user-score">Public Holiday</p>
                 <p class="user-score">${holiday.public}</p>
               </div>
@@ -43,3 +41,50 @@ const query = async function () {
 
 };
 query();
+
+const listen = function () {
+  DOMSelectors.searchForm.addEventListener("submit", function(e){
+    e.preventDefault();
+    DOMSelectors.grid.innerHTML = "";
+  const searchParams = DOMSelectors.searchArea.value;
+  const searchQuery = async function () {
+    try {
+        const response = await fetch(`https://holidayapi.com/v1/search/holidays?pretty&key=b2e5f5f9-a41f-4466-833d-a09cd17f4fd7&country=US&year=2020&query=${searchParams}`);
+        const data = await response.json();
+        console.log(data.holidays);
+        data.holidays.forEach((holiday) => {
+            DOMSelectors.grid.insertAdjacentHTML(
+              "beforeend", 
+              `<div class="movie-card">
+            <div class="movie-card-front">
+              
+              <h3 class="movie-card-header">${holiday.name}</h3>
+            </div>
+            <div class="movie-card-back">
+              
+              <div class="score-box">
+              
+                <p class="user-score">Public Holiday</p>
+                <p class="user-score">${holiday.public}</p>
+              </div>
+    
+              <div class="release-box">
+                <p class="release-date">Date</p>
+                <p class="release-date">${holiday.date}</p>
+                <p class="release-date">${holiday.weekday.date.name}</p>
+              </div>
+
+            </div>
+          </div>`);
+        })
+        
+    } catch (error) {
+        console.log(error);
+        alert("SOMETHING WRONG BOSS");
+    }
+
+};
+searchQuery();
+});
+}
+listen();
