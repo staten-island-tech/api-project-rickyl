ac7a3a6fa20ba3223123dee091c6a7afe5259f74
Ricky-L21 <71520397+Ricky-L21@users.noreply.github.com>
2021-05-27 19:22:11 -0400

    spelling monkaS

--- src/js/DOM.js
+++ src/js/DOM.js
@@ -1,6 +1,6 @@
 const DOMSelectors = {
-  grid: document.querySelector(".movie-grid"),
-  searchForm: document.getElementById("search-from"),
+  grid: document.querySelector(".holiday-grid"),
+  searchForm: document.getElementById("search-form"),
   searchArea: document.getElementById("search-area"),
 };
 
--- src/js/index.js
+++ src/js/index.js
@@ -1,90 +1,90 @@
 import { DOMSelectors } from "./DOM";
-import { genres } from "./genre";
 
 const key = "b2e5f5f9-a41f-4466-833d-a09cd17f4fd7";
 
-
 const query = async function () {
-    try {
-        const response = await fetch(`https://holidayapi.com/v1/holidays?pretty&key=b2e5f5f9-a41f-4466-833d-a09cd17f4fd7&country=US&year=2020`);
-        const data = await response.json();
-        console.log(data.holidays);
-        data.holidays.forEach((holiday) => {
-            DOMSelectors.grid.insertAdjacentHTML(
-              "beforeend", 
-              `<div class="movie-card">
-            <div class="movie-card-front">
+  try {
+    const response = await fetch(
+      `https://holidayapi.com/v1/holidays?pretty&key=${key}&country=US&year=2020`
+    );
+    const data = await response.json();
+    console.log(data.holidays);
+    data.holidays.forEach((holiday) => {
+      DOMSelectors.grid.insertAdjacentHTML(
+        "beforeend",
+        `<div class="holiday-card">
+            <div class="holiday-card-front">
               
-              <h3 class="movie-card-header">${holiday.name}</h3>
+              <h3 class="holiday-card-header">${holiday.name}</h3>
             </div>
-            <div class="movie-card-back">
+            <div class="holiday-card-back">
               
-              <div class="score-box">
+              <div class="info-box">
               
-                <p class="user-score">Public Holiday</p>
-                <p class="user-score">${holiday.public}</p>
+                <p class="public-info">Holiday for all?</p>
+                <p class="public-info">${holiday.public}</p>
               </div>
     
-              <div class="release-box">
-                <p class="release-date">Date</p>
-                <p class="release-date">${holiday.date}</p>
-                <p class="release-date">${holiday.weekday.date.name}</p>
+              <div class="date-box">
+                <p class="date">Date</p>
+                <p class="date">${holiday.date}</p>
+                <p class="date">${holiday.weekday.date.name}</p>
               </div>
 
             </div>
-          </div>`);
-        })
-    } catch (error) {
-        console.log(error);
-        alert("SOMETHING WRONG BOSS");
-    }
-
+          </div>`
+      );
+    });
+  } catch (error) {
+    console.log(error);
+    alert("SOMETHING WRONG BOSS");
+  }
 };
 query();
 
 const listen = function () {
-  DOMSelectors.searchForm.addEventListener("submit", function(e){
+  DOMSelectors.searchForm.addEventListener("submit", function (e) {
     e.preventDefault();
     DOMSelectors.grid.innerHTML = "";
-  const searchParams = DOMSelectors.searchArea.value;
-  const searchQuery = async function () {
-    try {
-        const response = await fetch(`https://holidayapi.com/v1/holidays?pretty&key=b2e5f5f9-a41f-4466-833d-a09cd17f4fd7&country=US&year=2020&search&query=${searchParams}`);
+    const searchParams = DOMSelectors.searchArea.value;
+    const searchQuery = async function () {
+      try {
+        const response = await fetch(
+          `https://holidayapi.com/v1/holidays?pretty&key=${key}&country=US&year=2020&search=${searchParams}`
+        );
         const data = await response.json();
-        console.log(data.holidays);
         data.holidays.forEach((holiday) => {
-            DOMSelectors.grid.insertAdjacentHTML(
-              "beforeend", 
-              `<div class="movie-card">
-            <div class="movie-card-front">
+          DOMSelectors.grid.insertAdjacentHTML(
+            "beforeend",
+            `<div class="holiday-card">
+            <div class="holiday-card-front">
               
-              <h3 class="movie-card-header">${holiday.name}</h3>
+              <h3 class="holiday-card-header">${holiday.name}</h3>
             </div>
-            <div class="movie-card-back">
+            <div class="holiday-card-back">
               
-              <div class="score-box">
+              <div class="info-box">
               
-                <p class="user-score">Public Holiday</p>
-                <p class="user-score">${holiday.public}</p>
+                <p class="public-info">Holiday for all?</p>
+                <p class="public-info">${holiday.public}</p>
               </div>
     
-              <div class="release-box">
-                <p class="release-date">Date</p>
-                <p class="release-date">${holiday.date}</p>
-                <p class="release-date">${holiday.weekday.date.name}</p>
+              <div class="date-box">
+                <p class="date">Date</p>
+                <p class="date">${holiday.date}</p>
+                <p class="date">${holiday.weekday.date.name}</p>
               </div>
 
             </div>
-          </div>`);
-        })
-        
-    } catch (error) {
+          </div>`
+          );
+        });
+      } catch (error) {
         console.log(error);
-        alert("SOMETHING WRONG BOSS");
-    }
-
+        alert("SOMETHING WRONG BOSS2");
+      }
+    };
+    searchQuery();
+  });
 };
-searchQuery();
-});
-}
 listen();
